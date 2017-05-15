24fe2943d796df24d866efaefb8faf050fffe21b
Deborah Kurata <deborahk@insteptech.com>
2017-05-15 07:39:51 -0700

    Update variable name to improve readability

--- src/app/movies/movie-filter.pipe.ts
+++ src/app/movies/movie-filter.pipe.ts
@@ -6,9 +6,9 @@ import { IMovie } from './movie';
 })
 export class MovieFilterPipe implements PipeTransform {
 
-    transform(value: IMovie[], filter: string): IMovie[] {
-        filter = filter ? filter.toLocaleLowerCase() : null;
-        return filter ? value.filter((movie: IMovie) =>
-            movie.title.toLocaleLowerCase().search(filter) !== -1) : value;
+    transform(value: IMovie[], filterBy: string): IMovie[] {
+        filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
+        return filterBy ? value.filter((movie: IMovie) =>
+            movie.title.toLocaleLowerCase().search(filterBy) !== -1) : value;
     }
 }
