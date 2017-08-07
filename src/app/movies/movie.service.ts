129e4feccd1e9b5b3fdbb9815874a2b6f592cf84
Deborah Kurata <deborahk@insteptech.com>
2017-08-07 14:51:27 -0700

    Code cleanup

--- src/app/movies/movie.service.ts
+++ src/app/movies/movie.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
-import { ErrorObservable } from "rxjs/observable/ErrorObservable";
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 import 'rxjs/add/observable/throw';
 
 import 'rxjs/add/operator/catch';
