65dd4c766219e7dc04cd0a69a953fd5502fb8625
Deborah Kurata <deborahk@insteptech.com>
2018-09-15 10:09:55 -0700

    Minor formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,15 @@ import { Component } from '@angular/core';
     selector: 'mh-root',
     template: `
         <nav class="navbar navbar-expand navbar-light bg-light">
-                <a class="navbar-brand">{{pageTitle}}</a>
-                <ul class="nav nav-pills">
-                    <li><a class="nav-link"  [routerLink]="['/welcome']">Home</a></li>
-                    <li><a class="nav-link"  [routerLink]="['/movies']">Movie List</a></li>
-                    <li><a class="nav-link"  [routerLink]="['/movies', 0, 'edit']">Add Movie</a></li>
-                </ul>
+          <a class="navbar-brand">{{pageTitle}}</a>
+          <ul class="nav nav-pills">
+            <li><a class="nav-link"  [routerLink]="['/welcome']">Home</a></li>
+            <li><a class="nav-link"  [routerLink]="['/movies']">Movie List</a></li>
+            <li><a class="nav-link"  [routerLink]="['/movies', 0, 'edit']">Add Movie</a></li>
+          </ul>
         </nav>
         <div class="container">
-            <router-outlet></router-outlet>
+          <router-outlet></router-outlet>
         </div>
      `,
   styleUrls: ['./app.component.css']
